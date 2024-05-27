5fa798bd2ab4920f43f2326e473c025c38e6883c
Matteo Rovellini <teorove04@gmail.com>
2024-05-27 09:34:09 +0200

    Add return to home button

--- src/app/services/page.service.ts
+++ src/app/services/page.service.ts
@@ -15,4 +15,10 @@ export class PageService {
     showRegisterPage(): boolean { return !this.isInLoginPage && this.isInRegisterPage && !this.as.isLogged }
     showAdminHomePage(): boolean { return !this.isInLoginPage && !this.isInRegisterPage && !this.isInCartPage && this.as.isAdmin }
     showCartPage(): boolean { return !this.isInLoginPage && !this.isInRegisterPage && !this.as.isAdmin && this.isInCartPage }
+
+    returnToHomePage(): void {
+        this.isInLoginPage = false;
+        this.isInRegisterPage = false;
+        this.isInCartPage = false;
+    }
 }
